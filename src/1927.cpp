5838a6c046472a60892ebcd2076fadb6f50f2db8
Jason Nordwick <jason.nordwick@gmail.com>
2026-08-23 18:40:36 -0400

    add 1927

---
+++ src/1927.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    pair<int,int> sumcnt(string_view sv) {
+        cout <<"sv=" << sv << endl;
+        int s = 0, c = 0;
+        for(auto x : sv) {
+            if(x == '?') c++;
+            else s += x - '0';
+        }
+        cout << "  s=" << s << " c=" << c << endl;
+        return make_pair(s, c);
+    }
+
+    bool sumGame(string const &m) {
+        auto [lsum, lcnt] = sumcnt(string_view(m).substr(0, m.size()/2));
+        auto [rsum, rcnt] = sumcnt(string_view(m).substr(m.size()/2));
+        return (lcnt + rcnt) & 1 || (rsum - lsum) != 9 * (lcnt - rcnt) / 2;
+    }
+};
